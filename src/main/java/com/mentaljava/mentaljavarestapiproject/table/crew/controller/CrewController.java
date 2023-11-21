5dbd3d70f852c82fafd70346960fe057c6b5b7c3
DESKTOP-OMBAS04\anty3 <anty3125@naver.com>
2023-11-21 18:52:01 +0900

    크루 모집상태조회/상세정보조회/모집글조회 모집글수정은 수정필요

--- src/main/java/com/mentaljava/mentaljavarestapiproject/table/crew/controller/CrewController.java
+++ src/main/java/com/mentaljava/mentaljavarestapiproject/table/crew/controller/CrewController.java
@@ -6,9 +6,7 @@ import com.mentaljava.mentaljavarestapiproject.table.crew.dto.CrewDTO;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -21,11 +19,47 @@ public class CrewController {
 
     public CrewController(CrewService crewService) { this.crewService = crewService; }
 
+    //전체 크루 리스트 조회
     @GetMapping("/list")
     public ResponseEntity<ResponseDTO> selectCrewList(){
         List<CrewDTO> crewList = crewService.findAllCrewList();
         System.out.println("crewList = " + crewList);
 
-        return ResponseEntity.ok().body(new ResponseDTO(HttpStatus.OK, "조회 성공", crewList));
+        return ResponseEntity.ok().body(new ResponseDTO(HttpStatus.OK, "전체 크루 리스트 조회 성공", crewList));
+    }
+
+    //모집상태1 조회
+    @GetMapping("/list/recruitmentstatusok")
+    public ResponseEntity<ResponseDTO> selectCrewListAboutRecruitmentStatusOk(){
+
+        return ResponseEntity.ok().body(new ResponseDTO(HttpStatus.OK, "모집 상태1 조회 성공", crewService.selectCrewListAboutRecruitmentStatusOk()));
+    }
+
+    //모집상태0 조회
+    @GetMapping("/list/recruitmentstatusno")
+    public ResponseEntity<ResponseDTO> selectCrewListAboutRecruitmentStatusNo(){
+
+        return ResponseEntity.ok().body(new ResponseDTO(HttpStatus.OK, "모집 상태0 조회 성공", crewService.selectCrewListAboutRecruitmentStatusNo()));
+    }
+
+    //크루 상세정보 조회
+    @GetMapping("/detail/{crewId}")
+    public ResponseEntity<ResponseDTO> selectCrewDetail(@PathVariable Integer crewId){
+
+        return ResponseEntity.ok().body(new ResponseDTO(HttpStatus.OK, "크루 상세정보 조회 성공", crewService.selectCrew(crewId)));
+    }
+
+    //크루 모집글 조회
+    @GetMapping("/detail/{crewId}/intro")
+    public ResponseEntity<ResponseDTO> selectCrewIntro(@PathVariable Integer crewId){
+
+        return ResponseEntity.ok().body(new ResponseDTO(HttpStatus.OK, "크루 소개글 조회 성공", crewService.selectCrewIntro(crewId)));
+    }
+
+    //크루 모집글 수정
+    @PutMapping(value = "/{crewId}/intro")
+    public ResponseEntity<ResponseDTO> updateCrewIntro(@PathVariable Integer crewId, @ModelAttribute CrewDTO crewDTO){
+
+        return ResponseEntity.ok().body(new ResponseDTO(HttpStatus.OK, "크루 소개글 수정 성공", crewService.updateCrewIntro(crewId, crewDTO)));
     }
 }
