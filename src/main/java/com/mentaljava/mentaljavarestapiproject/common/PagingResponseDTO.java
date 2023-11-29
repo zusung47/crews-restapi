b206799db4c03cf89265941bce867ca328cf2660
Hongse0 <hongseyoung981007@gmail.com>
2023-11-29 13:11:02 +0900

    공지사항 리스트 출력 페이징 처리

---
+++ src/main/java/com/mentaljava/mentaljavarestapiproject/common/PagingResponseDTO.java
@@ -0,0 +1,15 @@
+package com.mentaljava.mentaljavarestapiproject.common;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class PagingResponseDTO {
+
+    private Object data;
+    private PagingDTO pageInfo;
+
+}
--- src/main/java/com/mentaljava/mentaljavarestapiproject/table/notice/repository/NoticeRepository.java
+++ src/main/java/com/mentaljava/mentaljavarestapiproject/table/notice/repository/NoticeRepository.java
@@ -3,7 +3,10 @@ package com.mentaljava.mentaljavarestapiproject.table.notice.repository;
 import com.mentaljava.mentaljavarestapiproject.table.admin.dto.AdminDTO;
 import com.mentaljava.mentaljavarestapiproject.table.admin.entity.Admin;
 import com.mentaljava.mentaljavarestapiproject.table.notice.entity.Notice;
+import java.util.List;
 import java.util.Optional;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -13,4 +16,8 @@ public interface NoticeRepository extends JpaRepository<Notice, Integer> {
 
     @Query("SELECT a FROM Admin a where a.adminId=?1")
     Admin findByAdminId(String admin1);
+
+    List<Notice> findByDeleteStatus(int i);
+
+    Page<Notice> findByDeleteStatus(int i, Pageable paging);
 }
