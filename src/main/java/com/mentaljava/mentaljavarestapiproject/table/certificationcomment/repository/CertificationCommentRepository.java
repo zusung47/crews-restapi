f6b1261b2d1b8b5c7379ff7c9ff500162c2a0db4
Hongse0 <hongseyoung981007@gmail.com>
2023-12-16 19:23:42 +0900

    인증게시판 댓글 목록 페이징

--- src/main/java/com/mentaljava/mentaljavarestapiproject/table/certificationcomment/repository/CertificationCommentRepository.java
+++ src/main/java/com/mentaljava/mentaljavarestapiproject/table/certificationcomment/repository/CertificationCommentRepository.java
@@ -1,15 +1,15 @@
 package com.mentaljava.mentaljavarestapiproject.table.certificationcomment.repository;
 
 import com.mentaljava.mentaljavarestapiproject.table.certificationcomment.entity.CertificationComment;
-import com.mentaljava.mentaljavarestapiproject.table.certificationpost.dto.CertificationPostDTO;
 import com.mentaljava.mentaljavarestapiproject.table.certificationpost.entity.CertificationPost;
 import java.util.List;
-import java.util.Optional;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 public interface CertificationCommentRepository extends JpaRepository<CertificationComment, Integer> {
-
     List<CertificationComment> findByPostId(CertificationPost certificationPost);
 
+
+    Page<CertificationComment> findByPostId(CertificationPost certificationPost, Pageable paging);
 }
